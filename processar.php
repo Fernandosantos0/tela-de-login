8203420ace629a6549d3ffdb227863e7c1c74bb5
Fernando Santos <92190031+Fernandosantos0@users.noreply.github.com>
2022-07-17 08:59:20 -0300

    v7

---
+++ processar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="./assets/images/images-ok-cadastro.gif" alt="Garoto dando um joinha">
+        <?php
+            echo '<h1 style="color:green">Sucesso!<h1>';        
+        ?>
+    </body>
+</html>
